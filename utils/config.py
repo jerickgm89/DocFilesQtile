6a296874d55f208d0f12b83c31e4d0e1bfb588fc
JErickDev <jerickgm89@gmail.com>
2024-06-16 10:55:14 -0500

    Config: Modificacion para uso en QWERTY

---
+++ utils/config.py
@@ -0,0 +1,43 @@
+import json
+from dataclasses import asdict, dataclass
+from os import environ, getcwd
+from os.path import exists, expanduser, join
+
+
+@dataclass
+class Config:
+    bar: str = "shapes"
+    bar2: str = "shapes"
+    browser: str = "google-chrome-stable"
+    flameshot: str = "flameshot gui"
+    term: str | None = "alacritty"
+    term2: str = ""
+    wallpaper: str = "~/Pictures/Wallpapers/atardecer.jpg"
+
+    @property
+    def is_xephyr(self):
+        return int(environ.get("QTILE_XEPHYR", 0)) > 0
+
+    @staticmethod
+    def path() -> str:
+        xdg = expanduser("~/.config/qtile")
+        return xdg if exists(xdg) else getcwd()
+
+    @classmethod
+    def load(cls) -> "Config":
+        file = join(cls.path(), "cfg.json")
+        if not exists(file):
+            cls.generate(file)
+            return cls()
+        with open(file, "r") as f:
+            content = json.load(f)
+            return cls(**content)
+
+    @classmethod
+    def generate(cls, file: str):
+        with open(file, "w") as f:
+            content = asdict(cls())
+            f.write(json.dumps(content, indent=2))
+
+
+cfg = Config.load()
